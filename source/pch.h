630af902a4ef8109dc5d20be0437ba69e1a125a7
Daryn1 <68141961+Daryn1@users.noreply.github.com>
2020-07-25 15:14:21 +0600

    Delete pch.h

--- source/pch.h
+++
@@ -1,8 +0,0 @@
-// pch.h is required for successful compilation. We do not use pre-compiled headers in this project
-
-#ifndef PCH_H
-#define PCH_H
-
-// Add headers that you want to pre-compile here
-
-#endif //PCH_H
