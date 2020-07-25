bf8cccf976856f0a2d65072a1553336b43017423
Daryn1 <68141961+Daryn1@users.noreply.github.com>
2020-07-25 15:12:34 +0600

    Create pch.h

---
+++ source/pch.h
@@ -0,0 +1,8 @@
+// pch.h is required for successful compilation. We do not use pre-compiled headers in this project
+
+#ifndef PCH_H
+#define PCH_H
+
+// Add headers that you want to pre-compile here
+
+#endif //PCH_H
